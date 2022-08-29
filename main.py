5f53733bf00f54c2b21541aa98d5c171431d08bd
MD. Shimul <mdshemul480@gmail.com>
2022-08-28 22:30:26 -0700

    adding env config

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+
 def main():
     reseller_id = input("Enter reseller id: ")
+    site_url = getenv("SITE_LINK")
 
 
 if __name__ == "__main__":
