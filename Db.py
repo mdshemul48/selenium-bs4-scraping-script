b227d584aa7ca2454169c07b48384f9a63ce5c38
MD. Shimul <mdshemul480@gmail.com>
2022-08-31 18:16:56 +0600

    adding new db module

---
+++ Db.py
@@ -0,0 +1,40 @@
+import mysql.connector
+
+
+class DB:
+    def __init__(self, host, username, password, database) -> None:
+        self.db = mysql.connector.connect(
+            host=host,
+            user=username,
+            password=password,
+            database=database
+        )
+
+    def table(self, tableName):
+        self.tableName = tableName
+        return self
+
+    def insert(self, tableData: dict):
+        myCursor = self.db.cursor()
+
+        keys = list(tableData.keys())
+        values = list(tableData.values())
+        sqlKeys = ", ".join(keys)
+        sqlValueSet = ", ".join(["%s" for _ in range(len(keys))])
+
+        sql = f"INSERT INTO {self.tableName} ({sqlKeys}) VALUES ({sqlValueSet})"
+
+        myCursor.execute(sql, values)
+
+        self.db.commit()
+        myCursor.close()
+        return myCursor.rowcount
+
+
+if __name__ == "__main__":
+    db = DB("127.0.0.1", "root", "", "radius-circle")
+    print(db.table("nas").insert({
+        "nasname": "192.168.0.1",
+        "shortname": "thisIs",
+        "type": "others"
+    }))
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def main():
-    # reseller_id = input("Enter reseller Name: ")
 
 
 if __name__ == "__main__":
